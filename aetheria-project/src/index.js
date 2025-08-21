5f0405ad22f68e1f5f9c9a29cd94ddecf22dadc9
Yahia SAADE <saadeyahia29@gmail.com>
2025-08-21 23:25:09 +0200

    Add files via upload

--- aetheria-project/src/index.js
+++ aetheria-project/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // <-- ADD THIS LINE
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
